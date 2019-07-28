061955e8bd8ba3f690e5dd89c3e1e9cdc69607da
System Administrator <root@JESUSs-MacBook-Pro.local>
2019-07-27 20:17:41 -0500

    Added reduce function inside transformedIngredeints array to be able to check if array.lenght === 0 replace the content by a p tag that ask the user to add ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./Burgeringredient/Burgeringredient";
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey =>
-    [...Array(props.ingredients[igKey])].map((_, i) => (
-      <BurgerIngredient key={igKey + i} type={igKey} />
-    ))
-  );
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey =>
+      [...Array(props.ingredients[igKey])].map((_, i) => (
+        <BurgerIngredient key={igKey + i} type={igKey} />
+      ))
+    )
+    .reduce((arr, el) => arr.concat(el), []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
   console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
