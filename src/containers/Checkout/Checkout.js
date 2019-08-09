952eb8dd38a028ea2573f5c74ae5b0140ed684cd
System Administrator <root@JESUSs-MacBook-Pro.local>
2019-08-08 20:27:40 -0500

    added ContactData form hooked with bb state ingredients

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
-
+import { Route } from "react-router-dom";
+import ContactData from "./ContactData/ContactData";
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 1,
-      bacon: 1
-    }
+    ingredients: null,
+    totalPrice: 0
   };
   componentDidMount() {
-    const query = new URLSearchParams(this.props.search);
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    let price = 0;
+    for (let param of query.entries()) {
+      // ['salad', '1']
+      if (param[0] === "price") {
+        price = param[1];
+      } else {
+        ingredients[param[0]] = +param[1];
+      }
+    }
+    this.setState({ ingredients, totalPrice: price });
   }
   render() {
     return (
       <div>
         <CheckoutSummary ingredients={this.state.ingredients} />
+        <Route
+          path={`${this.props.match.url}/contact-data`}
+          render={() => (
+            <ContactData
+              ingredients={this.state.ingredients}
+              price={this.state.totalPrice}
+            />
+          )}
+        />
       </div>
     );
   }
---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import Button from "../../../components/UI/Button/Button";
+import classes from "./ContactData.module.css";
+
+class ContactData extends Component {
+  state = {
+    customer: null,
+    deliveryMethod: null,
+    loading: false
+  };
+  submitHandler = event => {
+    event.preventDefault();
+     this.setState({ loading: true });
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer: {
+        name: "Jesus Cova",
+        address: {
+          street: "Calle del fuego",
+          zipCode: 77710,
+          country: "Mexico"
+        },
+        email: "jesuscovam@gmail.com"
+      },
+      deliveryMethod: "fastest"
+    };
+    try {
+      const post = await axios.post("/order.json", order);
+      console.log(post);
+      this.setState({ loading: false, purchasing: false });
+    } catch (error) {
+      this.setState({ loading: false, purchasing: false });
+      console.log(error);
+    }
+  };
+  render() {
+    return (
+      <div className={classes.ContactData}>
+        <h4>Add your info</h4>
+        <form>
+          <input type="text" name="name" placeholder="Your Name" />
+          <input type="email" name="email" placeholder="Your Mail" />
+          <input type="text" name="street" placeholder="Your Street" />
+          <input type="text" name="postal" placeholder="Your Postal Code" />
+          <Button btnType="Success" click={this.submitHandler}>
+            Order
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ContactData;
